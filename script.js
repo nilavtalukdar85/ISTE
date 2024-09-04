4005a2a3ff8bd5d6ecd312d481f4b584943b5e8b
nilav-talukdar <nilavtalukdar49@gmail.com>
2024-09-05 01:03:12 +0530

    update script.js

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ document.getElementById('hamburger').addEventListener('click', function() {
     let navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 });
+let logoImg = document.querySelector('.logo-img');
+logoImg.addEventListener('click', () => {
+    location.reload();
+});
 // Navbar ends
