2ade5d97966ef46cfb4372ca5fe243a771b686f4
nilav-talukdar <nilavtalukdar49@gmail.com>
2024-09-05 00:53:26 +0530

    Navbar Done

---
+++ script.js
@@ -0,0 +1,6 @@
+// Navbar starts
+document.getElementById('hamburger').addEventListener('click', function() {
+    let navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
+// Navbar ends
