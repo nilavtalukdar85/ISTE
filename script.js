2ee585b23392ddacedfb57833c46358e4bf1f530
nilav-talukdar <nilavtalukdar49@gmail.com>
2024-09-05 22:28:12 +0530

    Fixed some minor issues

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.querySelector('.nav-links');
 
-    hamburger.addEventListener('click', function() {
-        navLinks.classList.toggle('active'); // Toggle the 'active' class to show/hide the menu
+    hamburger.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
     });
 });
-
 let logoImg = document.querySelector('.logo-img');
 logoImg.addEventListener('click', () => {
     location.reload();
