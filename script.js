b052d6b54d6ba52d60c0d9c15e9a6c1ac2b1892f
nilav-talukdar <nilavtalukdar49@gmail.com>
2024-09-14 20:49:17 +0530

    2nd page done and animations are also added

--- script.js
+++ script.js
@@ -17,4 +17,42 @@ let typed = new Typed('.auto-type', {
     backSpeed: 10,
     loop: true,
 });
+let x = document.querySelector('.content1-wrapper');
+console.log(x);
+let cards = document.querySelectorAll('.card');
+const animation = () => {
+    cards.forEach((card) => {
+        card.setAttribute('data-tilt', 'true');
+    });
+}
+
+// test code
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize Vanilla Tilt on card elements for specific screen sizes
+    function initializeTilt() {
+        if (window.innerWidth >= 710 && window.innerWidth <= 1080) {
+            VanillaTilt.init(document.querySelectorAll('.animate'), {
+                max: 25,
+                speed: 400,
+            });
+        }
+        if(window.innerWidth >= 1350) {
+            VanillaTilt.init(document.querySelectorAll('.primary'), {
+                max: 10,
+                speed: 400,
+            });
+        }
+    }
+
+    // Run on page load
+    initializeTilt();
+
+    // Run on window resize
+    window.addEventListener('resize', initializeTilt);
+});
+
+
+//test code
+
+
 AOS.init();
